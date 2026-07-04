bc83a63e9662e6a45b1c64a41f4cd2c645a3e05a
Tirth Patel <pateltirth1025@gmail.com>
2026-07-04 21:02:54 +0530

    Countries API added

--- script.js
+++ script.js
@@ -13,15 +13,21 @@
             clearInterval(timer)
             p.innerHTML="Go !"
             setTimeout(()=>{
-              startQuiz()
+              handleit()
             },1000)
          }
          
       }, 1000);
-     
-      function startQuiz()
+     function handleit(){
+       p.innerHTML="Quiz started !"
+      startQuiz()
+     }
+     async function startQuiz()
       {
-        p.innerHTML="Quiz started !Now fetch"
+       
+        let countryData=await fetch("https://api.restcountries.com/countries/v5?limit=25&pretty=1",{headers:{'Authorization':'Bearer rc_live_a200429ed56e4b7f8f16cdf5d21c2e50'}})
+        let response=await countryData.json()
+        console.log(response)
       }
      
 
